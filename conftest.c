ecefb16de227f7b8a5a1f58e1a8fcf1435ffb695
dojan <buech@mpi-inf.mpg.de>
2014-07-31 20:12:33 +0200

    first commit

---
+++ conftest.c
@@ -0,0 +1,52 @@
+/* confdefs.h.  */
+
+#define PACKAGE_NAME "GD"
+#define PACKAGE_TARNAME "gd"
+#define PACKAGE_VERSION "2.0.33"
+#define PACKAGE_STRING "GD 2.0.33"
+#define PACKAGE_BUGREPORT "gd@boutell.com"
+#define PACKAGE "gd"
+#define VERSION "2.0.33"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define STDC_HEADERS 1
+#define HAVE_ERRNO_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_ICONV 1
+#define ICONV_CONST const
+#define HAVE_ICONV_H 1
+#define HAVE_ICONV_T_DEF 1
+#define HAVE_LIBM 1
+#define HAVE_LIBZ 1
+#define HAVE_PNG_H 1
+#define HAVE_LIBPNG 1
+#define HAVE_LIBFREETYPE 1
+#define HAVE_FT2BUILD_H 1
+/* end confdefs.h.  */
+
+/* Override any gcc2 internal prototype to avoid an error.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+/* We use char because int might match the return type of a gcc2
+   builtin and then its argument prototype would still apply.  */
+char FcInit ();
+int
+main ()
+{
+FcInit ();
+  ;
+  return 0;
+}
